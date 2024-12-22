794e7e3a0ff954384c296468ec4bc8299dc9f34c
Cidergis <cidergis@gmail.com>
2024-12-21 20:36:40 -0800

    Initial commit for practice using practice doc code.

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Welcome to guessing game practice project!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    
+    //println!("The secret number is: {secret_number}");
+
+    loop {
+
+        println!("Please guess a number! OR ELSE!");
+    
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+
+        println!("You guessed: {}", guess);
+
+            match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too smol!"),
+            Ordering::Greater => println!("Too beeg!"),
+            Ordering::Equal => {
+                println!("GG");
+                break;
+                }
+            }
+        }
+}
