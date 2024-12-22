9bf4a9efc0afc4a047c60fab0116978e4837e04a
Trevor Lovell <trevorlovelldesign@gmail.com>
2024-12-21 21:58:23 -0700

    chore: format code

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
-use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Welcome to guessing game practice project!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    
+
     //println!("The secret number is: {secret_number}");
 
     loop {
-
         println!("Please guess a number! OR ELSE!");
-    
+
         let mut guess = String::new();
 
         io::stdin()
@@ -24,16 +23,15 @@ fn main() {
             Err(_) => continue,
         };
 
-
         println!("You guessed: {}", guess);
 
-            match guess.cmp(&secret_number) {
+        match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too smol!"),
             Ordering::Greater => println!("Too beeg!"),
             Ordering::Equal => {
                 println!("GG");
                 break;
-                }
             }
         }
+    }
 }
